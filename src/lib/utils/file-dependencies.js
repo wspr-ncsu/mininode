a7b7f230cbae48e25681538609144029e531a02f
eyang-pjrcorp <eyang@pjrcorp.com>
2023-02-17 10:56:38 -0800

    cleanup

--- src/lib/utils/file-dependencies.js
+++ src/lib/utils/file-dependencies.js
@@ -1,22 +1,22 @@
-const fs = require('fs');
-const resolve = require('resolve-from');
-const esprima = require('esprima');
+const fs = require("fs");
+const resolve = require("resolve-from");
+const esprima = require("esprima");
 const syntax = esprima.Syntax;
-const estraverse = require('estraverse');
+const estraverse = require("estraverse");
 
 module.exports = function allRequires(base, ast) {
   let resolveFrom = resolve.bind(base, null);
   let result = [];
   estraverse.traverse(ast, {
-    enter: function(node, parent) {
+    enter: function (node, parent) {
       // todo: add renamed requires
-      if (node.type === syntax.CallExpression && node.callee.name === 'require') {
-        
+      if (
+        node.type === syntax.CallExpression &&
+        node.callee.name === "require"
+      ) {
       }
     },
-    leave: function(node, parent) {
-
-    }
-  })
+    leave: function (node, parent) {},
+  });
   return result;
-}
+};
