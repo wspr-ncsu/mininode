c9a61184eb586bbfcf503ef5283cf832a298707f
eyang-pjrcorp <eyang@pjrcorp.com>
2023-02-23 17:48:35 -0800

    nit fix

--- src/index.js
+++ src/index.js
@@ -173,10 +173,6 @@ async function init() {
   _app.appname = packageJson.name;
   _app.version = packageJson.version;
   _app.type = packageJson.type;
-  
-  if (_app.type === "module") {
-    throw new Error("ES6_NOT_SUPPORTED");
-  }
   _app.path = location;
   _app.main = utils.entryPoint(location, packageJson.main);
 
