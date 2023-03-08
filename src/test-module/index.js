951f9c1606d4591449a3c06850879af048a32143
eyang-pjrcorp <eyang@pjrcorp.com>
2023-03-08 15:23:30 -0800

    clean up code, more readable name

--- src/test-module/index.js
+++ src/test-module/index.js
@@ -1,152 +1,133 @@
-const path = require('path');
-// path.resolve('..');
+const path = require("path");
 
 // normal (CORRECT!!!) usage of CommonJS exports and imports
 try {
-  const normal = require('normal');
+  const normal = require("normal");
   normal.x();
   normal.z();
-  // checkReductionStatus('normal', normal);
 } catch (error) {
-  console.log('normal.js failed', error);
+  console.log("normal.js failed", error);
 }
-
 
 // challenge #7: cross-reference
 try {
-  const crossref = require('crossref');
+  const crossref = require("crossref");
   crossref.use; //todo: is not working. Because I am not skipping unused exports.
-  // checkReductionStatus('crossref', crossref);
 } catch (error) {
-  console.log('crossref failed', error);
+  console.log("crossref failed", error);
 }
 
 // challenge #9: dynamic export
 try {
-  const dynamicExport = require('dynamic-export');
+  const dynamicExport = require("dynamic-export");
   dynamicExport.a();
   dynamicExport.c();
-  // checkReductionStatus('dynamic-export', dynamicExport);  
 } catch (error) {
-  console.error('dynamic-export failed', error);
+  console.error("dynamic-export failed", error);
 }
-
 
 // challenge #5: dynamic import
 try {
-  const dynamicImport = require('dynamic-import');
+  const dynamicImport = require("dynamic-import");
   dynamicImport.a();
-  // checkReductionStatus('dynamic-import', dynamicImport);
 } catch (error) {
-  console.error('dynamic-import failed', error);
+  console.error("dynamic-import failed", error);
 }
-
 
 // challenge #2: dynamic module
 try {
-  const dynamicModule = require('dynamic-module');
-  dynamicModule.a(); //todo: need to delete foo._b   
-  // checkReductionStatus('dynamic-usage', dynamicModule); 
+  const dynamicModule = require("dynamic-module");
+  dynamicModule.a(); //todo: need to delete foo._b
 } catch (error) {
-  console.error('dynamic-module failed', error);
+  console.error("dynamic-module failed", error);
 }
 
 // challenge #3: invisible-child-parent
 try {
-  const invisibleChildParent = require('invisible-child-parent');
-  invisibleChildParent.used();  
+  const invisibleChildParent = require("invisible-child-parent");
+  invisibleChildParent.used();
 } catch (error) {
-  console.error('invisible-child-parent', error);
+  console.error("invisible-child-parent", error);
 }
-
 
 // challenge #4: monkey patching
 try {
-  const monkeyPatching = require('monkey-patching');
+  const monkeyPatching = require("monkey-patching");
   monkeyPatching.used;
 } catch (error) {
-  console.error('monkey-patching failed', error);
+  console.error("monkey-patching failed", error);
 }
-
 
 // challenge #
 try {
-  const protoExport = require('proto-export');
+  const protoExport = require("proto-export");
   protoExport.a();
   protoExport.b();
   protoExport.d();
-  // checkReductionStatus('proto-export', protoExport);  
 } catch (error) {
-  console.error('proto-export failed', error);
+  console.error("proto-export failed", error);
 }
-
 
 // challenge #
 try {
-  const reExport = require('re-export');
+  const reExport = require("re-export");
   reExport.a;
   reExport.b;
-  // checkReductionStatus('re-export', reExport);  
+  // checkReductionStatus('re-export', reExport);
 } catch (error) {
-  console.error('re-export', error);
+  console.error("re-export", error);
 }
-
 
 // challenge #8: re-assigning export
 try {
   // todo: implement
-  const renameExport = require('rename-export');
+  const renameExport = require("rename-export");
   renameExport.a();
   renameExport.b();
   renameExport.c();
-  // checkReductionStatus('rename-export', renameExport);  
 } catch (error) {
-  console.error('rename-export failed', error);
+  console.error("rename-export failed", error);
 }
-
 
 // challenge #6: overwriting/renaming require
 try {
-  const renameRequire = require('rename-require');
-  renameRequire.used();  
+  const renameRequire = require("rename-require");
+  renameRequire.used();
 } catch (error) {
-  console.error('rename-require failed', error);
+  console.error("rename-require failed", error);
 }
-
 
 // challenge #: dynamic usage of module
 // fix: it is reducing dynamically used module
 try {
-  const dynamicModuleUsage = require('dynamic-module-usage'); 
-  dynamicModuleUsage.a();  
-  // checkReductionStatus('dynamic-module-usage', dynamicModuleUsage);
+  const dynamicModuleUsage = require("dynamic-module-usage");
+  dynamicModuleUsage.a();
 } catch (error) {
-  console.error('dynamic-module-usage failed', error);
+  console.error("dynamic-module-usage failed", error);
 }
 
 // challenge #: object-def-export
 try {
-  const objectDefExport = require('object-def-export');
+  const objectDefExport = require("object-def-export");
   objectDefExport.a();
   objectDefExport.b();
   // checkReductionStatus('object-def-export', objectDefExport);
 } catch (error) {
-  console.log('object-def-export failed', error);
+  console.log("object-def-export failed", error);
 }
 
-
 try {
-  const globalModule = require('global-module');
+  const globalModule = require("global-module");
   globalModule.use();
 } catch (error) {
-  console.log('global-module failed', error);
+  console.log("global-module failed", error);
 }
 
 try {
-  const functionExport = require('function-export');
+  const functionExport = require("function-export");
   const fe = new functionExport();
   fe.a();
   fe.b();
 } catch (error) {
-  console.log('function-export failed', error);
+  console.log("function-export failed", error);
 }
